c918ee115f490d5dd61697c22786cd0d85401fe5
mthoDev <mtho.dev@gmail.com>
2023-03-02 19:26:17 +0100

    V0.0.1
    Première version fonctionnelle exportée sur android.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,48 +2,82 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/* ordres du starter:
+ * variable "orders":
+ *      0: rien
+ *      1: On Your Marks
+ *      2: Get Set
+ *      3: Go !
+ *      4: False Start
+ */
 
 public class GameManager : MonoBehaviour
 {
-    public MouvementPlayer mouvementPlayer;
+    public SprinterManager sprinterManager;
     public Animator playerAnimator;
     public TextMeshProUGUI textMeshProStarterOrders;
+    public GameObject imageButton;
+    public Button restartButton;
 
     private int orders;
     private float tempsAffichageTexte = 1.0f;
-    private float t0;
+    private float tempsAttenteAvantDemmarage = 3.0f;
+    private float tempsAttente;
+    private float t0, tOrders;
+
 
     // Start is called before the first frame update
     void Start()
     {
         orders = 0;
+        tOrders = Time.time;
+        tempsAttente = tempsAttenteAvantDemmarage;
+        // Attache la méthode RestartGame au clic du bouton "Restart"
+        restartButton.onClick.AddListener(RestartGame);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetKeyDown("space") == true || Input.touchCount > 0)
+        if(orders < 3 && Time.time - tOrders > tempsAttente)
         {
-            orders++;
-
             switch (orders)
             {
-                case 1:
+                case 0:
+                    orders = 1;
+                    tOrders = Time.time;
+                    tempsAttente = 3.0f;
                     StarterOrder_onYourMarks();
-                    t0 = Time.time;
                     break;
 
-                case 2:
+                case 1:
+                    orders = 2;
+                    tOrders = Time.time;
+                    tempsAttente = Random.Range(1, 3);
                     StarterOrder_getSet();
-                    t0 = Time.time;
                     break;
-                case 3:
+                case 2:
+                    orders = 3;
                     StarterOrder_GO();
-                    t0 = Time.time;
                     break;
             }
-
+        }
+        else
+        {
+            if (orders == 3 && imageButton.activeSelf == false)
+            {
+                // Ajout d'une condition pour appeler la fonction uniquement si orders == 3 et que le bouton n'est pas déjà actif
+                ApparitionBouton();
+            }
+            else
+            {
+                if(orders == 4)
+                    StarterOrder_falseStart();
+            }
         }
 
         // On fait disparaitre le texte au bout de x secondes
@@ -58,6 +92,7 @@ public class GameManager : MonoBehaviour
     {
         textMeshProStarterOrders.text = "On your marks";
         textMeshProStarterOrders.gameObject.SetActive(true);
+        t0 = Time.time;
         playerAnimator.SetTrigger("OnYourMarks");
     }
 
@@ -66,6 +101,7 @@ public class GameManager : MonoBehaviour
 
         textMeshProStarterOrders.text = "Get Set";
         textMeshProStarterOrders.gameObject.SetActive(true);
+        t0 = Time.time;
         playerAnimator.ResetTrigger("OnYourMarks");
         playerAnimator.SetTrigger("GetSet");
     }
@@ -73,8 +109,49 @@ public class GameManager : MonoBehaviour
     {
         textMeshProStarterOrders.text = "Go !";
         textMeshProStarterOrders.gameObject.SetActive(true);
-        mouvementPlayer.MovePlayer(100.0f);
+        t0 = Time.time;
         playerAnimator.ResetTrigger("GetSet");
         playerAnimator.SetTrigger("Go");
+    }
+    void StarterOrder_falseStart()
+    {
+        textMeshProStarterOrders.text = "False Start !";
+        textMeshProStarterOrders.gameObject.SetActive(true);
+        t0 = Time.time;
+        playerAnimator.ResetTrigger("GetSet");
+        playerAnimator.SetTrigger("PlayerFalseStart");
+    }
+
+    void ApparitionBouton()
+    {
+        Vector3 nouvellePosition;
+
+        if (Random.Range(0, 2) == 1)
+            nouvellePosition = new Vector3(-350, 0, 0); // Bouton à gauche
+        else
+            nouvellePosition = new Vector3(350, 0, 0); // Bouton à droite
+
+        RectTransform rectTransformBouton = imageButton.GetComponent<RectTransform>();
+        rectTransformBouton.anchoredPosition = nouvellePosition;
+        imageButton.SetActive(true);
+    }
+
+    public void onClickButton()
+    {
+        sprinterManager.run();
+        imageButton.SetActive(false);
+    }
+
+    public void RestartGame()
+    {
+        // Réinitialisation des variables de jeu
+        orders = 0;
+        tOrders = Time.time;
+        tempsAttente = tempsAttenteAvantDemmarage;
+        sprinterManager.ResetPlayerPosition(); // méthode à créer pour remettre le joueur à la position de départ
+
+        // Recharge de la scène actuelle
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
     }
 }
--- Assets/Scripts/SprinterManager.cs
+++ Assets/Scripts/SprinterManager.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouvementPlayer : MonoBehaviour
+public class SprinterManager : MonoBehaviour
 {
-
     public float speed;
     public Rigidbody2D rigidBody;
     public Animator animator;
 
     private Vector3 velocity = Vector3.zero;
+    private double impulsion = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -20,9 +20,17 @@ public class MouvementPlayer : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float horizontalMovement = speed * Time.deltaTime;
+        if (impulsion > 0)
+        {
+            impulsion -= 0.1;
+        }
+        else
+        {
+            impulsion = 0;
+        }
+        double horizontalMovement = impulsion * speed * Time.deltaTime;
 
-        _MovePlayer(horizontalMovement);
+        _MovePlayer((float)horizontalMovement);
 
         float playerVelocity = Mathf.Abs(rigidBody.velocity.x);
         animator.SetFloat("speed", playerVelocity);
@@ -32,5 +40,17 @@ public class MouvementPlayer : MonoBehaviour
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rigidBody.velocity.y);
         rigidBody.velocity = Vector3.SmoothDamp(rigidBody.velocity, targetVelocity, ref velocity, .05f);
+    }
+
+    public void run()
+    {
+        impulsion += 4;
+    }
+
+    public void ResetPlayerPosition()
+    {
+        // Changer la position du joueur
+        transform.position = new Vector3(0, 0, 0);
+
     }
 }
